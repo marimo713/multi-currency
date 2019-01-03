d0551ea02c32def91c99b750b0a8ec934deefc2b
koki saito <koki@kokinoMacBook-puro.local>
2019-01-03 16:40:02 +0900

    add dollar test

---
+++ src/Dollar.php
@@ -0,0 +1,14 @@
+<?php
+
+class Dollar
+{
+    public $amount;
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+    public function times(int $multiplier)
+    {
+        $this->amount *= $multiplier;
+    }
+}
